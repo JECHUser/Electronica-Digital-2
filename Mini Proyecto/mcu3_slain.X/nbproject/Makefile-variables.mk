2be64049aae7581294bad33321dfb0e7c7f95ff1
JECHUser <66657576+JECHUser@users.noreply.github.com>
2021-02-18 10:56:35 -0600

    Entrega inicial
    Se agregó el proyecto con los códigos de los microcontroladores esclavos y maestro. Además, se incluyó el archivo de simulación de Proteus.

---
+++ Mini Proyecto/mcu3_slain.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=mcu3_slain.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/mcu3_slain.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mcu3slain.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mcu3slain.x.tar
